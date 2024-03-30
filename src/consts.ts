2b8af632b5746c57824ec9dd6bc985ea106b232b
SelfMadeSystem <sms@shoghisimon.cc>
2024-03-29 20:41:51 -0400

    fmt

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "SelfMadeSystem's Blog";
-export const SITE_DESCRIPTION = "A blog about tech, life and everything in between.";
+export const SITE_DESCRIPTION =
+  "A blog about tech, life and everything in between.";
