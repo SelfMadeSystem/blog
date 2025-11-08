9ab77b20e106c65f758dbb3f7679bdb7ee70778b
SelfMadeSystem <sms@shoghisimon.ca>
2025-11-08 11:15:54 -0500

    upgrade deps and whatnot

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.shoghisimon.cc/",
-  integrations: [mdx(), sitemap(), tailwind()],
+  site: "https://blog.shoghisimon.ca/",
+  integrations: [mdx(), sitemap(), react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
